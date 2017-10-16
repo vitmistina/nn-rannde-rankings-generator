ccae099bb08f12fe8ba70bb514f864a737f1fa74
Vit Mistina <vit.mistina@gmail.com>
2017-10-16 12:11:41 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:7.10.0
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+ARG NODE_ENV
+ENV NODE_ENV $NODE_ENV
+COPY package.json /usr/src/app/
+RUN npm install && npm cache clean
+
+EXPOSE 3000
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,97 +1,95 @@
-const webpack           = require('webpack');
-const path              = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const assetsDir       = path.join(__dirname, 'docs/public/assets');
-const nodeModulesDir  = path.join(__dirname, 'node_modules');
-const vendorsDir      = path.join(__dirname, 'src/app/vendors');
-const indexFile       = path.join(__dirname, 'src/app/index.js');
+const assetsDir = path.join(__dirname, "docs");
+const nodeModulesDir = path.join(__dirname, "node_modules");
+const vendorsDir = path.join(__dirname, "src/app/vendors");
+const indexFile = path.join(__dirname, "src/app/index.js");
 
 const SPLIT_STYLE = true;
 
 const config = {
   // devtool: '#source-map',
   entry: {
-    app: [
-      'babel-polyfill',
-      indexFile
-    ],
+    app: ["babel-polyfill", indexFile],
     vendor: [
-      'react',
-      'react-dom',
-      'react-tap-event-plugin',
-      'react-hot-loader',
-      'babel-polyfill',
-      'jquery',
-      'bootstrap/dist/js/bootstrap.min.js',
-      'redux',
-      'react-router-redux',
-      'redux-logger',
-      'redux-thunk',
-      'react-router',
-      'classnames',
-      'axios',
-      'js-base64',
-      'moment',
-      'react-bootstrap',
-      'react-modal',
-      'react-motion',
-      'react-notification'
+      "react",
+      "react-dom",
+      "react-tap-event-plugin",
+      "react-hot-loader",
+      "babel-polyfill",
+      "jquery",
+      "bootstrap/dist/js/bootstrap.min.js",
+      "redux",
+      "react-router-redux",
+      "redux-logger",
+      "redux-thunk",
+      "react-router",
+      "classnames",
+      "axios",
+      "js-base64",
+      "moment",
+      "react-bootstrap",
+      "react-modal",
+      "react-motion",
+      "react-notification"
     ]
   },
   output: {
-    path:     assetsDir,
-    filename: 'app.bundle.js'
+    path: assetsDir,
+    filename: "app.bundle.[chunkhash].js"
   },
   module: {
     rules: [
       {
-        test:     /\.jsx?$/,
-        exclude:  [nodeModulesDir, vendorsDir],
-        loader:   'babel-loader'
+        test: /\.jsx?$/,
+        exclude: [nodeModulesDir, vendorsDir],
+        loader: "babel-loader"
       },
       {
         test: /\.css$/,
-        use:  SPLIT_STYLE 
+        use: SPLIT_STYLE
           ? ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              {loader: 'css-loader', options: { importLoaders: 1 }},
-              'postcss-loader'
-            ]
-          })
+              fallback: "style-loader",
+              use: [
+                { loader: "css-loader", options: { importLoaders: 1 } },
+                "postcss-loader"
+              ]
+            })
           : [
-            'style-loader',
-            {loader: 'css-loader', options: { importLoaders: 1 }},
-            'postcss-loader'
-          ]
+              "style-loader",
+              { loader: "css-loader", options: { importLoaders: 1 } },
+              "postcss-loader"
+            ]
       },
       {
         test: /\.scss$/,
-        use:  SPLIT_STYLE 
-        ? ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {loader: 'css-loader', options: { importLoaders: 1 }},
-            'postcss-loader',
-            'sass-loader'
-          ]
-        })
-        : [
-          'style-loader',
-          {loader: 'css-loader', options: { importLoaders: 1 }},
-          'postcss-loader',
-          'sass-loader'
-        ]
+        use: SPLIT_STYLE
+          ? ExtractTextPlugin.extract({
+              fallback: "style-loader",
+              use: [
+                { loader: "css-loader", options: { importLoaders: 1 } },
+                "postcss-loader",
+                "sass-loader"
+              ]
+            })
+          : [
+              "style-loader",
+              { loader: "css-loader", options: { importLoaders: 1 } },
+              "postcss-loader",
+              "sass-loader"
+            ]
       },
       {
         test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
         use: [
           {
-            loader:  'url-loader',
+            loader: "file-loader",
             options: {
-              limit: 100000,
-              name: '[name].[ext]'
+              name: "[name].[hash].[ext]",
+              publicPath: "/public/assets/"
             }
           }
         ]
@@ -99,13 +97,24 @@ const config = {
     ]
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      title: "Zaměstnanecké fórum",
+      inject: false,
+      template: require("html-webpack-template"),
+      appMountId: "root",
+      lang: "cs-cz",
+      mobile: true,
+      baseHref: ""
+    }),
     getImplicitGlobals(),
     setNodeEnv(),
-    new ExtractTextPlugin('app.styles.css'),
+    new ExtractTextPlugin("app.styles.[contenthash].css"),
     new webpack.optimize.CommonsChunkPlugin({
-      name:     'vendor',
-      filename: 'app.vendor.bundle.js' 
-    })
+      name: "vendor",
+      filename: "app.vendor.bundle.[chunkhash].js"
+    }),
+    uglify()
   ]
 };
 /*
@@ -113,18 +122,41 @@ const config = {
 */
 function getImplicitGlobals() {
   return new webpack.ProvidePlugin({
-    $:      'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery'
+    $: "jquery",
+    jQuery: "jquery",
+    jquery: "jquery"
   });
 }
 
 function setNodeEnv() {
   return new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
+    "process.env": {
+      NODE_ENV: JSON.stringify("production")
     }
   });
 }
 
+function uglify() {
+  return new webpack.optimize.UglifyJsPlugin({
+    // Don't beautify output (enable for neater output)
+    beautify: false,
+    // Eliminate comments
+    comments: true,
+    // Compression specific options
+    compress: {
+      warnings: false,
+      // Drop `console` statements
+      drop_console: true
+    },
+    // Mangling specific options
+    mangle: {
+      // Don't mangle $
+      except: ["$"],
+      // Don't care about IE8
+      screw_ie8: true,
+      // Don't mangle function names
+      keep_fnames: false
+    }
+  });
+}
 module.exports = config;
